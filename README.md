930b572da1ac9875d95b1d6246e1f454fd344c4e
duyquangqa <duyquangnguyen1704@gmail.com>
2025-10-05 21:42:45 +0700

    refactor the main code and green 4ever always

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 
 # Welcome to Lamda Repository
 ### You will find in this repo the following stuff:
-* LAMDA EXPRESSION
-* WITH NETBEANS
+* The Math Library that offers multiple math functions (a Java-based source code)
+* Unit Testing source code with JUnit framework
+*GitHub Action to demonstrate the Continuos Integration technique
+*Regresstion Test
 * ...
 
 ### Screenshots
--- src/com/duyquang/mathutil/core/MathUtil.java
+++ src/com/duyquang/mathutil/core/MathUtil.java
@@ -21,18 +21,43 @@ public class MathUtil {
     //20 giai thừa 18 con số 0, vừa kịp cho đủ kiểu long của JAVA
     //21 giai thừa tràn hiểu long
     //bài này quy ước tính n! trong khoảng từ 0..20
+//    public static long getFactorial(int n){
+//        if(n < 0 || n > 20){
+//            throw new IllegalArgumentException("Invalid Argument. N must be between 0..20");
+//           
+//        }
+//        if(n == 0 || n == 1){
+//            return 1; //kết thúc cuộc chơi nếu nhận những đâu vào đặc biệt
+//        }
+//       long product = 1; //tích nhân dồn, thuật toán con heo đất
+//        for (int i = 2; i <= n; i++) {
+//            product *= i;
+//        }
+//        return product;
+//    }
+    
+    
+    
+    //Học về đệ quy - RECURSION
+    //Hiện tượng gọi lại chính mình với 1 quy mô khác 
+    //Con búp bê Nga, giống nhau và lồng trong nhau
+    //búp bê to -> nhỏ -> nhỏ hơn ->.......-> điểm dừng
+    
+    //tinh giai thừa 6!
+    //n! = 1.2.3.4.5.6....n
+    //6! = 6 x 5!
+    //5! = 5 x 4!
+    //4! = 4 x 3!
+    //3! = 3 x 2!
+    //2! = 2 x 1! //điểm dừng
+    //quy ước 1! = 0! = 1;
+    //CHỐT LẠI: n! = n x (n-1)!
     public static long getFactorial(int n){
-        if(n < 0 || n > 20){
-            throw new IllegalArgumentException("Invalid Argument. N must be between 0..20");
-           
-        }
-        if(n == 0 || n == 1){
+       
+        if(n == 0 || n == 1)
             return 1; //kết thúc cuộc chơi nếu nhận những đâu vào đặc biệt
-        }
-       long product = 1; //tích nhân dồn, thuật toán con heo đất
-        for (int i = 2; i <= n; i++) {
-            product *= i;
-        }
-        return product;
+        
+        return n * getFactorial(n - 1); //công thức đệ quy
     }
+    
 }
